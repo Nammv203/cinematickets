dbd9d30c699c16eeebb5076f2511fb7500d9e23d
Bùi Đức Duy <102569075+buiduyba@users.noreply.github.com>
2024-11-19 02:56:19 +0700

    fix update user

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -1,46 +1,39 @@
 <?php
 
-namespace App\Rules;
+namespace App\Http\Requests;
 
-use App\Models\CinemaRoom;
-use Closure;
-use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
 
-class CinemaRoomCodeUniqueRule implements ValidationRule
+class UserUpdateRequest extends FormRequest
 {
-    protected $cinema_id;
-
-    protected $room_code;
-
-
-    protected $room_id;
-
-    public function __construct($cinema_id, $room_code, $room_id)
-    {
-        $this->cinema_id = $cinema_id;
-        $this->room_code = $room_code;
-        $this->room_id = $room_id;
-    }
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    // public function authorize(): bool
+    // {
+    //     return false;
+    // }
 
     /**
-     * Run the validation rule.
+     * Get the validation rules that apply to the request.
      *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function validate(string $attribute, mixed $value, Closure $fail): void
+    public function rules(): array
     {
-        try {
-            $cinemaId = $this->cinema_id;
-            $roomCode = $this->room_code;
-            $roomId = $this->room_id;
-
-            $roomCodeOfCinemaExists = CinemaRoom::where('cinema_id',$cinemaId)
-                ->where('room_code',$roomCode)
-                ->when($roomId, function ($query, $roomId) {
-                    return $query->where('id','!=',$roomId);
-                })
-                ->exists();
-
+        return [
+            // 'name' => 'required',
+            // 'birthday' => 'required',
+            'role_id' => 'required',
+        ];
     }
+
+    public function attributes()
+    {
+        return [
+            'name' => 'Tên người dùng',
+            'birthday' => 'Ngày sinh',
+            'role_id' => 'Vai trò',
+        ];
     }
 }
