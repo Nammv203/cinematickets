2e5ff6e6afcdea956f2159c185034d7525b779d3
Thevurightthere <thevu2103@gmail.com>
2024-11-26 12:44:51 +0700

    update luồng đặt vé - chưa có thanh toán online

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -48,6 +48,8 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(\App\Repositories\LocationDistrictRepository::class, \App\Repositories\LocationDistrictRepositoryEloquent::class);
 
         $this->app->bind(\App\Repositories\SchedulePublishFilmRepository::class, \App\Repositories\SchedulePublishFilmRepositoryEloquent::class);
+        $this->app->bind(\App\Repositories\TicketOrderRepository::class, \App\Repositories\TicketOrderRepositoryEloquent::class);
+        $this->app->bind(\App\Repositories\TicketOrderItemRepository::class, \App\Repositories\TicketOrderItemRepositoryEloquent::class);
         //:end-bindings::end-bindings:
     }
 }
